afb6f3c8d10f52120ef0c4c32dfbc45f1e886d34
shivank55 <shivankjain555@gmail.com>
2024-07-17 10:50:41 +0530

    creted a server with http module

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
 const http = require("http")  
+const fs = require("fs")
 
 const myServer = http.createServer((req,res)=>{ 
-   console.log("We Have Recieved A Request")
-   res.end("Hello From The Server")
+   const log = `${Date.now()} ${req.url} New Request Received\n`
+//NON_BLOCKING   
+    fs.appendFile("log.txt",log,(err,data)=>{
+        switch(req.url){ 
+            case '/' : res.end("HomePage");
+            break;
+            case '/about': res.end("I am Shivank Jain The Best MERN STACK CODER");
+            break;
+            default:  res.end("404 Not Found")
+        }
+            
+    })  
 })
 
 
