b86b17ed403335e46548eca5dc7db496ff5939cc
shivank55 <shivankjain555@gmail.com>
2024-07-17 10:07:42 +0530

    creted a server with http module

--- index.js
+++ index.js
@@ -4,3 +4,6 @@ const myServer = http.createServer((req,res)=>{
    console.log("We Have Recieved A Request")
    res.end("Hello From The Server")
 })
+
+
+myServer.listen(8000,()=>{console.log("Server Has Started")})
